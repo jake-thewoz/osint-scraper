f252dc07e69b22daacb08f7c67744c3d86ed2cd2
jake-thewoz <jake.thewoz@gmail.com>
2020-12-27 16:26:09 -0600

    Added a basic webbrowser.open function, so that I can start testing good dorking combinations and other sites.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-import sys, requests
+import sys, requests, webbrowser
 
+# Check to see if the user used the command correctly
 if len(sys.argv)!=2:
 	sys.exit("Usage: osint-scraper <email>")
 
-
+# Checking available email searches in browser
+webbrowser.open('https://thatsthem.com/email/' + sys.argv[1])
+webbrowser.open('https://www.skymem.info/srch?q=' + sys.argv[1] + '&ss=home')
+webbrowser.open('https://www.google.com/search?q="' + sys.argv[1] + '"+filetype%3Apdf')
